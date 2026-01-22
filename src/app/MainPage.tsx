1784a44ad50c3934d9657214b8c318d3dc13cab9
seojin <seojini00@naver.com>
2026-01-22 10:24:55 +0900

    mainpage 배경색상 변경 및 안내 문구 추가

--- src/app/MainPage.tsx
+++ src/app/MainPage.tsx
@@ -2,9 +2,10 @@ import Button from "@/components/common/Button";
 
 function MainPage() {
   return (
-    <div className="min-h-screen bg-gray-100">
+    <div className="min-h-screen bg-EEE9E5 flex flex-col items-center">
       <h1 className="text-3xl font-bold text-center py-8">Boogle</h1>
-      <Button size = "large">로그인하기</Button>
+      <p className="text-sm  text-center">신촌 및 홍대 지역 대학생을 위한 <br/> 맞춤형 카페 추천 서비스</p>
+      <Button size = "large" >로그인하기</Button>
 
     </div>
   );
