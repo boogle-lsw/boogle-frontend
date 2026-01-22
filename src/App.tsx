f43df32b67c78a7da1be0c2d87f48188a8489b23
seojin <seojini00@naver.com>
2026-01-22 09:58:44 +0900

    mainpage router 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import Button from "@/components/common/Button"; 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainPage from './app/MainPage';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold text-center py-8">Boogle</h1>
-      <Button>ㅎㅇ</Button>
-      <Button variant="brownDark" size="large">하이</Button>
-
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/app/MainPage.tsx
@@ -0,0 +1,13 @@
+import Button from "@/components/common/Button"; 
+
+function MainPage() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-center py-8">Boogle</h1>
+      <Button size = "large">로그인하기</Button>
+
+    </div>
+  );
+}
+
+export default MainPage;
