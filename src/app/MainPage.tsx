7e0512c68423f59c2de8cf98af4a4df2a545ddbc
seojin <seojini00@naver.com>
2026-01-22 14:08:30 +0900

    logo를 여백이 더 작은 크기의 이미지로 변경

--- src/app/MainPage.tsx
+++ src/app/MainPage.tsx
@@ -4,7 +4,7 @@ import Logo from "@/assets/images/mainLogo.png";
 function MainPage() {
   return (
     <div className="min-h-screen bg-[#EEE9E5] flex flex-col items-center">
-      <img src={Logo} alt="Boogle 로고" className="w-80 mb-1"/>
+      <img src={Logo} alt="Boogle 로고" className="w-40 mb-1"/>
       <h1 className="text-3xl font-bold text-center py-8">Boogle</h1>
       <p className="text-sm  text-center">신촌 및 홍대 지역 대학생을 위한 <br/> 맞춤형 카페 추천 서비스</p>
 
